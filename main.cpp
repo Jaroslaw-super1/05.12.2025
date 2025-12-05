0984af98fb2a647a657ef0fea0347ade2d2cbda0
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-12-05 14:39:00 +0500

    p_t and f_t

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+namespace topit
+{
+  struct p_t
+  {
+    int x, y;
+  };
+  bool operator == (p_t, p_t);
+  bool operator != (p_t, p_t);
+
+  struct f_t
+  {
+    p_t aa, bb;
+  };
+  
+
+  struct IDraw
+  {
+    // = 0 - Значит метод чисто виртуальный и ма не предоставим реализацию
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
+    virtual ~IDraw();
+  };
+}
+
+
 int main()
 {
-  
+
+  using topit::p_t;
+  p_t a{1, 0}, b{1, 0};
+
+  std::cout << (a != b) << '\n';
+
+  return 0;
 }
+
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b)
+{
+  return !(a == b);
+}
+
+
